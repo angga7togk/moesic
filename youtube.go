d24feca05c143b7d4d30f2c8c35d5eaf6aba224d
Angga7Togk <kiplihode123321@gmail.com>
2025-08-05 23:24:20 +0700

    [feat] add crawler code

---
+++ youtube.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+type Youtube struct {
+	Duration float64
+	Url      string
+}
+
+func GetYoutube(videoURL string) (*Youtube, error) {
+	var binary string
+	switch runtime.GOOS {
+	case "linux":
+		binary = "./bin/yt-dlp_linux"
+	case "windows":
+		binary = "./bin/yt-dlp_win.exe"
+	case "darwin":
+		binary = "./bin/yt-dlp_macos"
+	default:
+		return nil, fmt.Errorf("unsupported OS: %s", runtime.GOOS)
+	}
+
+	cmd := exec.Command(binary, "-f", "bestaudio[ext=m4a]", "-j", videoURL)
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, fmt.Errorf("yt-dlp error: %v", err)
+	}
+
+	var data struct {
+		Duration float64 `json:"duration"`
+		URL      string  `json:"url"`
+		Formats  []struct {
+			Ext      string  `json:"ext"`
+			ACodec   string  `json:"acodec"`
+			VCodec   string  `json:"vcodec"`
+			FormatID string  `json:"format_id"`
+			URL      string  `json:"url"`
+		} `json:"formats"`
+	}
+	err = json.Unmarshal(output, &data)
+	if err != nil {
+		return nil, fmt.Errorf("json parse error: %v", err)
+	}
+
+	for _, format := range data.Formats {
+		if format.Ext == "m4a" && format.VCodec == "none" {
+			return &Youtube{
+				Duration: data.Duration,
+				Url:      format.URL,
+			}, nil
+		}
+	}
+
+	return nil, fmt.Errorf("m4a audio format not found")
+}
