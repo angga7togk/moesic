2b96fb80a4d2520b9fe90091933c4688cd4a6dac
Angga7Togk <kiplihode123321@gmail.com>
2025-08-09 16:33:34 +0700

    [yt-dlp] change result to audio url only

--- yt-dlp.go
+++ yt-dlp.go
@@ -1,51 +1,23 @@
 package main
 
 import (
-	"encoding/json"
+	"bytes"
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
-type Youtube struct {
-	Duration int64
-	Url      string
-}
 
-func GetAudio(videoURL string) (*Youtube, error) {
+func GetAudio(videoURL string) (string, error) {
 	path := getYtDlpPath()
-	cmd := exec.Command(path, "-f", "bestaudio[ext=m4a]", "-j", videoURL)
-	output, err := cmd.Output()
+	cmd := exec.Command(path, "-f", "bestaudio[ext=m4a]", "-g", videoURL)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
 	if err != nil {
-		return nil, fmt.Errorf("yt-dlp error: %v", err)
+		return "", fmt.Errorf("yt-dlp error: %v", err)
 	}
-
-	var data struct {
-		Duration int64    `json:"duration"`
-		URL      string `json:"url"`
-		Formats  []struct {
-			Ext      string `json:"ext"`
-			ACodec   string `json:"acodec"`
-			VCodec   string `json:"vcodec"`
-			FormatID string `json:"format_id"`
-			URL      string `json:"url"`
-		} `json:"formats"`
-	}
-	err = json.Unmarshal(output, &data)
-	if err != nil {
-		return nil, fmt.Errorf("json parse error: %v", err)
-	}
-
-	for _, format := range data.Formats {
-		if format.Ext == "m4a" && format.VCodec == "none" {
-			// * gw kurangin 1 soalnya biasanya, durasi 4:26 trus progrssnya cuman nyampe 4:25
-			// * ya sementara solusinya cuman ini :)
-			dur := data.Duration - 1 
-			return &Youtube{
-				Duration: dur,
-				Url:      format.URL,
-			}, nil
-		}
-	}
-
-	return nil, fmt.Errorf("m4a audio format not found")
+	url := strings.TrimSpace(out.String())
+	return url, nil
 }
+
