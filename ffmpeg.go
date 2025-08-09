68cc87dfcdcb2edc5b606a5dfe3593a382c8ec5f
Angga7Togk <kiplihode123321@gmail.com>
2025-08-09 16:34:40 +0700

    [ffmpeg] add realtime duration

--- ffmpeg.go
+++ ffmpeg.go
@@ -8,6 +8,22 @@ import (
 	"strings"
 )
 
+func durationToSeconds(s string) (int64, error) {
+	var h, m int
+	var sec float64
+	_, err := fmt.Sscanf(s, "%02d:%02d:%f", &h, &m, &sec)
+	if err != nil {
+		return 0, err
+	}
+	total := int64(h)*3600 + int64(m)*60 + int64(sec)
+	return total, nil
+}
+
+var (
+	globalCurrentTime     int64 = 0 // * global progres music player
+	globalCurrentDuration int64 = 0
+)
+
 func play(url string) *exec.Cmd {
 	cmd := exec.Command("ffplay", "-nodisp", "-autoexit", "-loglevel", "info", "-infbuf", url)
 
@@ -31,14 +47,28 @@ func play(url string) *exec.Cmd {
 				break
 			}
 
-			// * buat dengerin progress audio nya, entah itu video audio apapu itu
+			if strings.Contains(line, "Duration:") {
+				// * ambil string setelah kata "Duration:"
+				parts := strings.SplitN(line, "Duration:", 2)
+				if len(parts) > 1 {
+					after := strings.TrimSpace(parts[1]) // ? "00:03:23.96, start: 0.000000, bitrate: 129 kb/s"
+					// * potong sampai koma pertama
+					durationStr := strings.SplitN(after, ",", 2)[0]
+					f, err := durationToSeconds(durationStr)
+					if err == nil {
+						globalCurrentDuration = f
+					}
+				}
+			}
+
+			// * buat dengerin progress audio nya, entah itu video audio apapu itu 
 			if strings.Contains(line, "A-V:") || strings.Contains(line, "M-A:") || strings.Contains(line, "M-V:") {
 				fields := strings.Fields(line)
 				if len(fields) > 0 {
 					timestampStr := fields[0]
 					f, err := strconv.ParseFloat(timestampStr, 64)
 					if err == nil {
-						globalPlayerTime = int64(f)
+						globalCurrentTime = int64(f)
 					}
 				}
 			}
